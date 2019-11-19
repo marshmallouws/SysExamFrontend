446c91e88cb37961a8597ec2450996dc3f5a8d0e
Casper Kruse Olesen <casperolesen@me.com>
2019-11-19 12:43:57 +0100

    Added code to Event component + added a new file EventDetails

---
+++ src/EventDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventDetails = () => {
+    return (
+        <div>
+            <p>EventDetails..</p>
+        </div>
+    )
+}
+
+export default EventDetails;
--- src/EventList.js
+++ src/EventList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Event from './Event';
 
 const EventList = ({data}) => {
     return (
         <div>
             <p>{data}</p>
+            <table>
+                <thead>
+                    <th>Event</th>
+                </thead>
+                <tbody>
+                    <tr><Event event="event 1" /></tr>
+                    <tr><Event event="event 2" /></tr>
+                </tbody>
+            </table>
         </div>
     )
 }
