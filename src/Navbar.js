8b9283c81387c3882db6f4d7b62abff86666fab2
vl48 <wegokr@gmail.com>
2019-11-19 12:21:30 +0100

    logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,47 +1,57 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import facade from "./apifacade";
-import {
-  NavLink,
-  Link
-} from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function Navbar(props) {
-    const [isAuthenticating, setIsAuthenticating] = useState(true);
-    const [isAuthenticated, userHasAuthenticated] = useState(false);
-  
-    const logout = () => {
-      facade.logout();
-      userHasAuthenticated(false);
-    }
-  
-    useEffect(() => {
-      onLoad();
-    }, []);
-  
-    function onLoad() {
-      userHasAuthenticated(true);
-      setIsAuthenticating(false);
-    }
-  
-    return (
-      !isAuthenticating &&
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  const logout = () => {
+    facade.logout();
+    userHasAuthenticated(false);
+  };
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  function onLoad() {
+    userHasAuthenticated(true);
+    setIsAuthenticating(false);
+  }
+
+  return (
+    !isAuthenticating && (
       <nav className="header">
         <ul>
+          <li className="logo">
+            <img src="./assets/logo.png" />
+            ESportsTraveller
+          </li>
           <li>
-            <NavLink exact to="/">Home</NavLink>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
           </li>
           <li>
             <NavLink to="/user">Roles</NavLink>
           </li>
-  
-          {facade.getToken() == null ? (<li style={{ float: "right" }}>
-            <Link to="/login">Log in</Link>
-          </li>) : (<li style={{ float: "right" }}><Link to="/" onClick={logout}>Log out</Link></li>)
-          }
-  
+
+          {facade.getToken() == null ? (
+            <li style={{ float: "right" }}>
+              <Link to="/login">Log in</Link>
+            </li>
+          ) : (
+            <li style={{ float: "right" }}>
+              <Link to="/" onClick={logout}>
+                Log out
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
-    );
-  }
+    )
+  );
+}
 
-  export default Navbar;
+export default Navbar;
